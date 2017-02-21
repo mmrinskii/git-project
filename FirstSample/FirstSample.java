1f1df8ddfb98765be7d2b9169e0638a18312a87b
maxim <mmrinskiy@gmail.com>
2017-02-21 17:53:00 +0200

    commit to branch master

--- FirstSample/FirstSample.java
+++ FirstSample/FirstSample.java
@@ -7,8 +7,11 @@ public class FirstSample
 {
    public static void main(String[] args)
    {
+testing merging
       System.out.println("We will not use 'Hello, World! We will use hello world Maxim, Hello branch Master'");
       double x = 10000.0 / 3.0;
       System.out.printf("%8.2f", x);
+testing merging
    }
 }
+testing of merging

992fd01a866ff9089874b1434b1de4611fa1157a
maxim <mmrinskiy@gmail.com>
2017-02-21 08:34:07 +0200

    FirstSample modification

--- FirstSample/FirstSample.java
+++ FirstSample/FirstSample.java
@@ -7,7 +7,7 @@ public class FirstSample
 {
    public static void main(String[] args)
    {
-      System.out.println("We will not use 'Hello, World!'");
+      System.out.println("We will not use 'Hello, World! We will use hello world Maxim'");
       double x = 10000.0 / 3.0;
       System.out.printf("%8.2f", x);
    }

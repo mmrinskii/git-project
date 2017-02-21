0bfa9d57b26f351b675cbb8082dc0cb8d20f43df
maxim <mmrinskiy@gmail.com>
2017-02-21 07:39:09 +0200

    init

---
+++ FirstSample/FirstSample.java
@@ -0,0 +1,14 @@
+/**
+ * This is the first sample program in Core Java Chapter 3
+ * @version 1.01 1997-03-22
+ * @author Gary Cornell
+ */
+public class FirstSample
+{
+   public static void main(String[] args)
+   {
+      System.out.println("We will not use 'Hello, World!'");
+      double x = 10000.0 / 3.0;
+      System.out.printf("%8.2f", x);
+   }
+}
